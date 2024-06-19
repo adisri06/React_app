dfd421b6506a44ce78e63df0277c3a1bc9b8392e
adisri06 <adi.sri06@gmail.com>
2024-06-19 19:15:14 +0530

    testing new app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input type='text' placeholder='name'/><br/><br/>
+      <input type='password' placeholder='password'/><br/><br/>
+      <input type='submit' nvalue= "log"/><br/><br/>
+
+
+    </form>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//reactdom.render is used to render an element or component in virtual dom
+//what needs to render and where to render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
